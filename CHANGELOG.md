8c358fd06115d861de750f677a494c27a40df27a
Dillon Regimbal <dillonregi@gmail.com>
2020-06-01 14:29:12 -0400

    Added changelog, Small QoL improvements

---
+++ CHANGELOG.md
@@ -0,0 +1,45 @@
+# ChangeLog
+
+## SteamGifts Giveaway Entry
+
+### 1.0.3 - Small QoL Improvements
+
+- Added a short delay between entries to avoid a soft-ban
+- Stops sending entries when out of points to avoid a soft-ban
+- Updates the button text to provide more info while sending entries
+- New Features (off by default)
+  - New method of giveaway entry that clicks the [ESGST](https://github.com/rafaelgssa/esgst) "Enter" buttons to enter giveaways, to allow the hiding of entered giveaways
+  - Can automatically close the modals that pop up after entering giveaways
+  - Can now press the [ESGST](https://github.com/rafaelgssa/esgst) "Refresh all pages" button after entering giveaways
+
+### 1.0.2 - Fixed links
+
+- Fixed the [GreasyFork](https://greasyfork.org/en) link to the [UserScript](https://greasyfork.org/en/scripts/403895-steamgifts-com-giveaway-entry)
+
+### 1.0.1 - Initial Release
+
+- Initial Release
+- Allows bulk entry of [SteamGifts](https://www.steamgifts.com) giveaways
+- Respects/ignores giveaways that were hidden by [ESGST](https://github.com/rafaelgssa/esgst)
+
+## Bundle Helper
+
+### 2.0.2 - Updated GitHub link
+
+- Update GitHub link
+
+### 2.0.1 - Initial Release
+
+- Initial Release
+- Shows which games you already own on Steam, on other sites
+
+## Free Steam Packages Redeemer
+
+### 1.0.1 - Updated Github link
+
+- Update GitHub link
+
+### 1.0.0 - Initial Release
+
+- Initial Release
+- Helps bulk redeem free Steam packages
--- SteamGiveawayEntry.user.js
+++ SteamGiveawayEntry.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            SteamGifts.com Giveaway Entry
-// @version         1.0.2
+// @version         1.0.3
 // @author          Dillon Regimbal
 // @namespace       https://dillonr.com
 // @description     Easily enter all visible giveaways. Pairs well with ESGST (https://github.com/rafaelgssa/esgst)
@@ -26,6 +26,13 @@
     let button_background_colour = '#4F7A26'
     let button_foreground_colour = '#FFFFFF'
     let button_font_size = '18px'
+
+    let esgst_entry_style = false
+    let esgst_refresh_pages_after_entry = false
+    let esgst_close_modals_after_entry = false
+    let esgst_submit_button_selector = '.esgst-elgb-button .form__submit-button'
+    let esgst_refresh_button_id = 'esgst-esRefreshAll'
+    let esgst_modal_close_button_selector = '.esgst-popup-close'
 
     // #endregion
 
@@ -85,7 +92,19 @@
 
     // #endregion
 
+    // #region Global variables
+
+    // ESGST "Enter" buttons
+    let esgst_Giveaways = []
+
+    // Giveaway IDS to enter
     let giveawayIDs = []
+
+    // Points that can be used to enter giveaways
+    // Updates when a request to SteamGifts is made
+    let entryPoints = 400
+
+    // #endregion
 
     function enterGiveaway() {
         if (giveawayIDs.length > 0) {
@@ -99,15 +118,26 @@
                     },
                     onload: function (response) {
                         let e = JSON.parse(response.responseText)
+                        if (typeof e.points !== 'undefined') {
+                            entryPoints = e.points
+                        }
                         if (e.type === 'success') {
-                            console.log('Entry Count: ' + e.entry_count + ', Points: ' + e.points)
+                            enterBtnA.textContent = `Entry Count: ${e.entry_count}, Points: ${e.points}`
                         } else {
                             console.warn(e)
                         }
-                        if (giveawayIDs.length > 0) {
-                            enterGiveaway()
-                        } else {
+
+                        if (giveawayIDs.length <= 0) {
+                            // All giveaways entered
                             enterBtnA.textContent = no_giveaways_text
+                            finishedEnteringGiveaways()
+                        } else if (entryPoints <= 0) {
+                            // No more points
+                            enterBtnA.textContent = `You have ${entryPoints} points`
+                            finishedEnteringGiveaways()
+                        } else {
+                            // Continue entering giveaways
+                            setTimeout(enterGiveaway, 200)
                         }
                     }
                 })
@@ -116,25 +146,74 @@
         }
     }
 
+    function finishedEnteringGiveaways() {
+        if (esgst_refresh_pages_after_entry) {
+            document.getElementById(esgst_refresh_button_id).click()
+        }
+        if (esgst_close_modals_after_entry) {
+            setTimeout(esgst_ClearModal, 300)
+        }
+    }
+
+    function esgst_FindGiveaways() {
+        esgst_Giveaways = []
+        document.querySelectorAll(esgst_submit_button_selector)
+            .forEach(giveaway => {
+                esgst_Giveaways.push(giveaway)
+            })
+
+        enterBtnA.textContent = `Enter ${esgst_Giveaways.length} Giveaways!`
+    }
+
+    function esgst_SubmitGiveaway() {
+        if (esgst_Giveaways.length > 0) {
+            enterBtnA.textContent = `${esgst_Giveaways.length} Giveaways Remaining!`
+            let giveaway = esgst_Giveaways.pop()
+            giveaway.click()
+            // Continue entering giveaways
+            setTimeout(esgst_SubmitGiveaway, 200)
+        } else {
+            enterBtnA.textContent = no_giveaways_text
+            finishedEnteringGiveaways()
+        }
+    }
+
+    function esgst_ClearModal() {
+        document.querySelectorAll(esgst_modal_close_button_selector)
+            .forEach(closeButton => {
+                closeButton.click()
+            })
+    }
+
     function submitGiveaways() {
-        enterBtnA.textContent = `Entering ${giveawayIDs.length} Giveaways!`
-        divButton.addEventListener('click', findGiveaways)
         divButton.removeEventListener('click', submitGiveaways)
-        enterGiveaway()
+        divButton.addEventListener('click', findGiveaways)
+        if (esgst_entry_style) {
+            enterBtnA.textContent = `Entering ${esgst_Giveaways.length} Giveaways!`
+            esgst_SubmitGiveaway()
+        } else {
+            enterBtnA.textContent = `Entering ${giveawayIDs.length} Giveaways!`
+            enterGiveaway()
+        }
     }
 
     function findGiveaways() {
-        document.querySelectorAll(giveaway_selector)
-            .forEach(giveaway => {
-                let id = giveaway.href.replace('https://www.steamgifts.com', '').replace('/giveaway/', '').split('/')[0]
-                if (!giveawayIDs.includes(id)) {
-                    giveawayIDs.push(id)
-                }
-            })
+        if (esgst_entry_style) {
+            esgst_FindGiveaways()
+        } else {
+            giveawayIDs = []
+            document.querySelectorAll(giveaway_selector)
+                .forEach(giveaway => {
+                    let id = giveaway.href.replace('https://www.steamgifts.com', '').replace('/giveaway/', '').split('/')[0]
+                    if (!giveawayIDs.includes(id)) {
+                        giveawayIDs.push(id)
+                    }
+                })
 
-        enterBtnA.textContent = `Enter ${giveawayIDs.length} Giveaways!`
-        divButton.addEventListener('click', submitGiveaways)
+            enterBtnA.textContent = `Enter ${giveawayIDs.length} Giveaways!`
+        }
         divButton.removeEventListener('click', findGiveaways)
+        divButton.addEventListener('click', submitGiveaways)
     }
 
     let divButton = document.createElement('div')
